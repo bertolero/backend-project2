ea67f2803f1a97d18f4cb8912ad9edb0473d133e
Bertol <bertol.pinheiro@gmail.com>
2020-06-13 16:09:22 -0300

    Config mongoose connection.
    Signed-off-by: Bertol <bertol.pinheiro@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { app } from "./app";
 import * as http from "http";
-import { MongoHelper } from "./helper/mongo.helper";
+import * as mongoose from "mongoose";
 
 const PORT = 8080;
+const MONGO_URI = "mongodb://127.0.0.1:27017";
 const server = http.createServer(app);
 
 server.listen(PORT);
 server.on("listening", async () => {
   console.log(`Listening on port ${PORT}`);
-  try {
-    await MongoHelper.connect("mongodb://127.0.0.1:27017");
+  mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.connection.on("open", () => {
     console.error("Connected");
-  } catch (err) {
+  });
+  mongoose.connection.on("error", (err: any) => {
     console.error(err);
-  }
+  });
 });
